c931d9c48c6441b01c4e03ba2842f6c17cdd45f9
kar2905 <kar2905@gmail.com>
2013-11-21 17:00:10 -0800

    css fix

--- includes/header.php
+++ includes/header.php
@@ -17,7 +17,7 @@
 
         <link rel="stylesheet" href="<?=$site_url?>css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="<?=$site_url?>/css/MyFontsWebfontsKit.css">
-        <link rel="stylesheet" href="<?=$site_url?>css/main.css">
+        <link rel="stylesheet" href="<?=$site_url?>css/main.css?v=1">
         <link rel="stylesheet" href="<?=$site_url?>css/font-awesome.min.css">
         <script src="<?=$site_url?>js/vendor/modernizr-2.6.2.min.js"></script>
         <script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
